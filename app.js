800c7e6cacdcc156e6347428a09b6a1842b015d6
asaqa988 <asaqa001@gmail.com>
2023-09-26 20:00:28 +0300

    update

---
+++ app.js
@@ -0,0 +1,69 @@
+// let names = [
+//   "ahmad",
+//   "omar",
+//   "eman",
+//   "rawan",
+//   "rami",
+//   "feras",
+//   "ghadeer",
+//   "eyad",
+//   "mohammad",
+//   "zaid",
+//   "nisreen",
+//   "basem"
+// ];
+
+// var smallLetters = [
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "f",
+//   "g",
+//   "h",
+//   "i",
+//   "j",
+//   "k",
+//   "l",
+//   "m",
+//   "n",
+//   "o",
+//   "p",
+//   "q",
+//   "r",
+//   "s",
+//   "t",
+//   "u",
+//   "v",
+//   "w",
+//   "x",
+//   "y",
+//   "z",
+// ];
+
+// let myRandomIndex = Math.floor(Math.random() * smallLetters.length);
+// let Genratedletter = smallLetters[myRandomIndex];
+
+
+// console.log(Genratedletter)
+// function checkTheName() {
+//   for (let i = 0; i < names.length; i++) {
+//     console.log(i)
+//     if (names[i][0] === Genratedletter) {
+//       console.log(
+//         `${i} this name ${names[i]} is starting with the letter  ${Genratedletter}`
+//       );
+//     }
+//   }
+// }
+// checkTheName();
+
+
+let var1 = "10"
+
+let var2 = 10 
+
+if(var1 ===var2) {
+    console.log("mar7ba")
+}
